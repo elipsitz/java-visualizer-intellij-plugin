64961fad511d3bfaf1cb8603a4df85587ecd0462
Eli Lipsitz <eli.lipsitz@gmail.com>
2019-01-06 19:47:43 -0600

    Add placeholder text in visualization panel before any trace is loaded

--- src/main/java/com/aegamesi/java_visualizer/plugin/MainPane.java
+++ src/main/java/com/aegamesi/java_visualizer/plugin/MainPane.java
@@ -4,20 +4,32 @@ import com.aegamesi.java_visualizer.model.ExecutionTrace;
 import com.aegamesi.java_visualizer.ui.VisualizationPanel;
 import com.intellij.ui.components.JBScrollPane;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import java.awt.BorderLayout;
 
 class MainPane extends JPanel {
+	private JLabel placeholderLabel;
 	private VisualizationPanel viz;
 
 	MainPane() {
 		setLayout(new BorderLayout());
 
-		viz = new VisualizationPanel();
-		add(new JBScrollPane(viz));
+		String text = "No execution trace loaded: make sure you've stopped on a breakpoint.";
+		placeholderLabel = new JLabel(text, SwingConstants.CENTER);
+		add(placeholderLabel);
 	}
 
 	void setTrace(ExecutionTrace trace) {
+		if (viz == null) {
+			remove(placeholderLabel);
+			viz = new VisualizationPanel();
+			JBScrollPane scrollPane = new JBScrollPane(viz);
+			scrollPane.setBorder(null);
+			add(scrollPane);
+			revalidate();
+		}
 		viz.setTrace(trace);
 	}
 }
